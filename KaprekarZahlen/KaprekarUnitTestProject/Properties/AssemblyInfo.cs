d05bc36433d654374a5738479de27fa9f2395b7e
steckart <stephan.eckart@comhard.de>
2019-08-13 08:46:26 +0200

    Projekte angelegt

---
+++ KaprekarZahlen/KaprekarUnitTestProject/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KaprekarUnitTestProject")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KaprekarUnitTestProject")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d3dd1fd0-902e-4515-8cee-2e0af5a5dbbb")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KaprekarZahlen/KaprekarUnitTestProject/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KaprekarUnitTestProject
+{
+    [TestClass]
+    public class KaprekarZahlenTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
