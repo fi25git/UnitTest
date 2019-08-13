e82feab8cfbc054b675adf8cee088903ff77eece
servicevs2015 <servicevs2015@o365.comhard.de>
2019-08-13 14:29:41 +0200

    neu

--- KaprekarZahlen/UnitTestPerson/UnitTest1.cs
+++ KaprekarZahlen/UnitTestPerson/UnitTest1.cs
@@ -7,8 +7,9 @@ namespace UnitTestPerson
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestNeu()
         {
+            
         }
     }
 }
