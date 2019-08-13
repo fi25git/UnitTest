d84855ca84c74fad7f93f7ce1ff1c1d3c5010b60
ckb\ASperling <alexsperlin_72@yahoo.de>
2019-08-13 14:23:33 +0200

    abc

--- KaprekarZahlen/KaprekarUnitTestProject/UnitTest1.cs
+++ KaprekarZahlen/KaprekarUnitTestProject/UnitTest1.cs
@@ -10,87 +10,87 @@ namespace KaprekarUnitTestProject
         [TestMethod]
         public void Test1()
         {
-            Assert.AreEqual(1, Kaprekar.getNumber(1));
+            Assert.AreEqual(1, ASperling.getNumber(1));
         }
         [TestMethod]
         public void Test2()
         {
-            Assert.AreEqual(9, Kaprekar.getNumber(2));
+            Assert.AreEqual(9, ASperling.getNumber(2));
         }
         [TestMethod]
         public void Test3()
         {
-            Assert.AreEqual(45, Kaprekar.getNumber(3));
+            Assert.AreEqual(45, ASperling.getNumber(3));
         }
         [TestMethod]
         public void Test4()
         {
-            Assert.AreEqual(55, Kaprekar.getNumber(4));
+            Assert.AreEqual(55, ASperling.getNumber(4));
         }
         [TestMethod]
         public void Test5()
         {
-            Assert.AreEqual(99, Kaprekar.getNumber(5));
+            Assert.AreEqual(99, ASperling.getNumber(5));
         }
         [TestMethod]
         public void Test6()
         {
-            Assert.AreEqual(297, Kaprekar.getNumber(6));
+            Assert.AreEqual(297, ASperling.getNumber(6));
         }
         [TestMethod]
         public void Test7()
         {
-            Assert.AreEqual(703, Kaprekar.getNumber(7));
+            Assert.AreEqual(703, ASperling.getNumber(7));
         }
         [TestMethod]
         public void Test8()
         {
-            Assert.AreEqual(999, Kaprekar.getNumber(8));
+            Assert.AreEqual(999, ASperling.getNumber(8));
         }
         [TestMethod]
         public void Test9()
         {
-            Assert.AreEqual(2223, Kaprekar.getNumber(9));
+            Assert.AreEqual(2223, ASperling.getNumber(9));
         }
         [TestMethod]
         public void Test10()
         {
-            Assert.AreEqual(2728, Kaprekar.getNumber(10));
+            Assert.AreEqual(2728, ASperling.getNumber(10));
         }
         [TestMethod]
         public void Test11()
         {
-            Assert.AreEqual(4873, Kaprekar.getNumber(11));
+            Assert.AreEqual(4879, ASperling.getNumber(11));
         }
         [TestMethod]
         public void Test12()
         {
-            Assert.AreEqual(4950, Kaprekar.getNumber(12));
+            Assert.AreEqual(4950, ASperling.getNumber(12));
         }
         [TestMethod]
         public void Test13()
         {
-            Assert.AreEqual(5050, Kaprekar.getNumber(13));
+            Assert.AreEqual(5050, ASperling.getNumber(13));
         }
         [TestMethod]
         public void Test14()
         {
-            Assert.AreEqual(5292, Kaprekar.getNumber(14));
+            Assert.AreEqual(5292, ASperling.getNumber(14));
         }
         [TestMethod]
         public void Test15()
         {
-            Assert.AreEqual(7272, Kaprekar.getNumber(15));
+            Assert.AreEqual(7272, ASperling.getNumber(15));
         }
         [TestMethod]
         public void Test16()
         {
-            Assert.AreEqual(7777, Kaprekar.getNumber(16));
+            Assert.AreEqual(7777, ASperling.getNumber(16));
         }
         [TestMethod]
         public void Test17()
         {
-            Assert.AreEqual(9999, Kaprekar.getNumber(17));
+            Assert.AreEqual(9999, ASperling.getNumber(17));
         }
     }
 }
---
+++ KaprekarZahlen/KaprekarZahlen/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaprekarZahlen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int zahl = 148149;
+            Console.WriteLine(ASperling.getNumber(3));
+            //Console.WriteLine("Zahl: " + zahl);
+            //ASperling.isKaprekar(zahl);
+            Console.Read();
+        }
+    }
+}
