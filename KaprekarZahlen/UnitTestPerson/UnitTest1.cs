cc0b2c7b85831176e17d6528ab096927b52ebcc4
UnitTest-Ivan <servicevs2015@o365.comhard.de>
2019-08-13 14:53:19 +0200

    Test

--- KaprekarZahlen/UnitTestPerson/UnitTest1.cs
+++ KaprekarZahlen/UnitTestPerson/UnitTest1.cs
@@ -16,5 +16,38 @@ namespace UnitTestPerson
             Assert.AreEqual(false, neuling.Verheiratet);
             Assert.AreEqual(false, neuling.Verwitwet);
         }
+
+        [TestMethod]
+        public void TestNewPersonIsGeschieden()
+        {
+            Person.Person geschiedene = new Person.Person();
+            Assert.AreEqual(false, geschiedene.Ledig);
+            Assert.AreEqual(true, geschiedene.Geschieden);
+            Assert.AreEqual(false, geschiedene.Verheiratet);
+            Assert.AreEqual(false, geschiedene.Verwitwet);
+
+        }
+
+        [TestMethod]
+        public void TestNewPersonIsVerheireitet()
+        {
+            Person.Person verheiratete = new Person.Person();
+            Assert.AreEqual(false, verheiratete.Ledig);
+            Assert.AreEqual(false, verheiratete.Geschieden);
+            Assert.AreEqual(true, verheiratete.Verheiratet);
+            Assert.AreEqual(false, verheiratete.Verwitwet);
+           
+        }
+        
+        [TestMethod]
+        public void TestNewPersonIsVerwitwet()
+        {
+            Person.Person verwitwet = new Person.Person();
+            Assert.AreEqual(false, verwitwet.Ledig);
+            Assert.AreEqual(false, verwitwet.Geschieden);
+            Assert.AreEqual(false, verwitwet.Verheiratet);
+            Assert.AreEqual(true, verwitwet.Verwitwet);
+
+        }
     }
 }
