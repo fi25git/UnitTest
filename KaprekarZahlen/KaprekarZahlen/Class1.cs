9a8839e86d7d383ac378f2d0912eeaefdcfa31dd
servicevs2015 <servicevs2015@o365.comhard.de>
2019-08-13 14:23:21 +0200

    neu

--- KaprekarZahlen/KaprekarZahlen/Class1.cs
+++ KaprekarZahlen/KaprekarZahlen/Class1.cs
@@ -8,11 +8,33 @@ namespace KaprekarZahlen
 {
     public class Kaprekar
     {
-        public static int getNumber(int n)
+        public static bool getNumber(int n)
         {
-            if (n == 1) return 1;
-            if (n == 2) return 9;
-            return 0;
+            if (n == 1)
+                return true;
+            int sq_n = n * n;
+            int count_digits = 0;
+            while (sq_n != 0)
+            {
+                count_digits++;
+                sq_n /= 10;
+            }
+            sq_n = n * n;
+            for (int r_digits = 1; r_digits < count_digits; r_digits++)
+            {
+                int eq_parts = (int)Math.Pow(10, r_digits);
+                if (eq_parts == n)
+                    continue;
+                int sum = sq_n / eq_parts + sq_n % eq_parts;
+                if (sum == n)
+                {
+                    Console.WriteLine("Kaprekar");
+                    return true;
+                }
+
+            }
+            Console.WriteLine("nicht Kaprekar");
+            return false;
         }
     }
 }
