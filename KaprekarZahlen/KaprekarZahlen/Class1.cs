7a1795de8d3e27149b4f7cb9d96a3ff52a964656
jklann@ckb.comhard.de <jklann@comhard.de>
2019-08-13 14:22:55 +0200

    fdhgdf

--- KaprekarZahlen/KaprekarZahlen/Class1.cs
+++ KaprekarZahlen/KaprekarZahlen/Class1.cs
@@ -7,7 +7,40 @@ using System.Threading.Tasks;
 namespace KaprekarZahlen
 {
     public class Kaprekar
-    {
+    {   
+        public static int Paprika(int n)
+        {
+            int zahl = 0;
+            int zaehler = 1;
+            if (n == 1) { return n; }
+            for (int i = 1; i < n; i++)
+            {
+                
+                int quadrat = zaehler * zaehler;
+                if ((quadrat.ToString().Length % 2) == 0)
+                {
+                    int teiler = (quadrat.ToString().Length / 2);
+                    int zwerg = (Convert.ToInt32(quadrat.ToString().Substring(0, teiler))) + (Convert.ToInt32(quadrat.ToString().Substring(teiler, teiler)));
 
+                    if (zwerg == zaehler)
+                    {
+                        zahl++;
+                        if (n == zahl)
+                        {
+                            return zaehler;
+                        }
+                        zaehler++;
+                    }
+                }
+                else
+                {
+                    zaehler++;
+                    break;
+                }
+            }
+            return n; 
+            
+        }
+        
     }
 }
---
+++ KaprekarZahlen/KaprekarZahlen/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KaprekarZahlen 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Test");
+            Console.WriteLine(Kaprekar.Paprika(2)); 
+            Console.ReadLine();
+        }
+    }
+}
