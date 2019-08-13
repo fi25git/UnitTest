87cc884b9222187fe1ad7c75a0946d1578b68bde
servicevs2015 <servicevs2015@o365.comhard.de>
2019-08-13 09:20:19 +0200

    tests

--- KaprekarZahlen/KaprekarUnitTestProject/UnitTest1.cs
+++ KaprekarZahlen/KaprekarUnitTestProject/UnitTest1.cs
@@ -22,6 +22,75 @@ namespace KaprekarUnitTestProject
         {
             Assert.AreEqual(45, Kaprekar.getNumber(3));
         }
-
+        [TestMethod]
+        public void Test4()
+        {
+            Assert.AreEqual(55, Kaprekar.getNumber(4));
+        }
+        [TestMethod]
+        public void Test5()
+        {
+            Assert.AreEqual(99, Kaprekar.getNumber(5));
+        }
+        [TestMethod]
+        public void Test6()
+        {
+            Assert.AreEqual(297, Kaprekar.getNumber(6));
+        }
+        [TestMethod]
+        public void Test7()
+        {
+            Assert.AreEqual(703, Kaprekar.getNumber(7));
+        }
+        [TestMethod]
+        public void Test8()
+        {
+            Assert.AreEqual(999, Kaprekar.getNumber(8));
+        }
+        [TestMethod]
+        public void Test9()
+        {
+            Assert.AreEqual(2223, Kaprekar.getNumber(9));
+        }
+        [TestMethod]
+        public void Test10()
+        {
+            Assert.AreEqual(2728, Kaprekar.getNumber(10));
+        }
+        [TestMethod]
+        public void Test11()
+        {
+            Assert.AreEqual(4873, Kaprekar.getNumber(11));
+        }
+        [TestMethod]
+        public void Test12()
+        {
+            Assert.AreEqual(4950, Kaprekar.getNumber(12));
+        }
+        [TestMethod]
+        public void Test13()
+        {
+            Assert.AreEqual(5050, Kaprekar.getNumber(13));
+        }
+        [TestMethod]
+        public void Test14()
+        {
+            Assert.AreEqual(5292, Kaprekar.getNumber(14));
+        }
+        [TestMethod]
+        public void Test15()
+        {
+            Assert.AreEqual(7272, Kaprekar.getNumber(15));
+        }
+        [TestMethod]
+        public void Test16()
+        {
+            Assert.AreEqual(7777, Kaprekar.getNumber(16));
+        }
+        [TestMethod]
+        public void Test17()
+        {
+            Assert.AreEqual(9999, Kaprekar.getNumber(17));
+        }
     }
 }
