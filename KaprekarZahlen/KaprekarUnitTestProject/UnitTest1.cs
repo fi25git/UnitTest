99a376126bb4b08baff7d2d847a3493449f086f7
mthiele <mthiele4@web.de>
2019-08-13 09:04:20 +0200

    h

--- KaprekarZahlen/KaprekarUnitTestProject/UnitTest1.cs
+++ KaprekarZahlen/KaprekarUnitTestProject/UnitTest1.cs
@@ -7,8 +7,19 @@ namespace KaprekarUnitTestProject
     public class KaprekarZahlenTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void Test1()
         {
+            Assert.AreEqual(1, Kaprekar.getNumber(1));
+        }
+        [TestMethod]
+        public void Test2()
+        {
+            Assert.AreEqual(9, Kaprekar.getNumber(2));
+        }
+        [TestMethod]
+        public void Test3()
+        {
+            Assert.AreEqual(45, Kaprekar.getNumber(3));
         }
     }
 }
--- KaprekarZahlen/KaprekarZahlen/Class1.cs
+++ KaprekarZahlen/KaprekarZahlen/Class1.cs
@@ -8,6 +8,11 @@ namespace KaprekarZahlen
 {
     public class Kaprekar
     {
-
+        public static int getNumber(int n)
+        {
+            if (n == 1) return 1;
+            if (n == 2) return 2;
+            return 0;
+        }
     }
 }
