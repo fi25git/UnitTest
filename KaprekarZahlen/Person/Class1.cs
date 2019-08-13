86eae968328322845791f929f86cc6320c86586d
mthiele <mthiele4@web.de>
2019-08-13 14:58:11 +0200

    b

--- KaprekarZahlen/Person/Class1.cs
+++ KaprekarZahlen/Person/Class1.cs
@@ -8,26 +8,68 @@ namespace Person
 {
     public class Person
     {
-        public bool Ledig { get; }
-        public bool Verheiratet { get; }
-        public bool Geschieden{get; }
-        public bool Verwitwet { get; }
+        public bool Ledig { get { return ledig; } }
+        public bool Verheiratet { get { return verheiratet; } }
+        public bool Geschieden{ get { return geschieden; } }
+        public bool Verwitwet { get { return verwitwet; } }
+
+        bool ledig;
+        bool verheiratet;
+        bool geschieden;
+        bool verwitwet;
 
         public void heiraten()
         {
-            
+            if (ledig == true)
+            {
+                ledig = false;
+                verheiratet = true;
+            }
+
+            if (geschieden==true)
+            {
+                geschieden = false;
+                verheiratet = true;
+            }
+
+            if (verwitwet==true)
+            {
+                verwitwet = false;
+                verheiratet = true;
+            }
         }
+
         public void annulieren()
         {
+            if (verheiratet==true)
+            {
+                verheiratet = false;
+                ledig = true;
+            }
 
+            if (geschieden==true)
+            {
+                geschieden = false;
+                verheiratet = true;
+            }
         }
+
         public void scheiden()
         {
-
+            if(verheiratet==true)
+            {
+                verheiratet = false;
+                geschieden = true;
+            }
         }
+
         public void PartnerTod()
         {
-
+            if(verheiratet==true)
+            {
+                verheiratet = false;
+                verwitwet = true;
+            }
         }
     }
 }
