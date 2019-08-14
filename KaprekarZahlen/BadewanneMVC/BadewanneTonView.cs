d944a1823d1a43907b2d0708c78ae958eab40b3d
servicevs2015 <servicevs2015@o365.comhard.de>
2019-08-14 13:24:38 +0200

    ViewTon

---
+++ KaprekarZahlen/BadewanneMVC/BadewanneTonView.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Speech.Synthesis;
+
+namespace BadewanneMVC
+{
+
+    class BadewanneTonView : IBadewanneView
+    {
+        SpeechSynthesizer speaker = new SpeechSynthesizer();
+
+        public event EventHandler ViewChanged;
+        int fuellstand;
+
+
+        public void fuellen(int liter)
+        {
+            fuellstand += liter;
+        }
+        public void ablassen(int liter)
+        {
+            fuellstand += liter;
+        }
+        public void TexttoSpeech()
+        {
+            speaker.Rate = 1;
+            speaker.Volume = 100;
+            speaker.SelectVoiceByHints(VoiceGender.Female, VoiceAge.Adult);
+            speaker.SpeakAsync("Der Füllstand beträgt: " + fuellstand);
+        }
+       
+            
+            
+
+    }
+}
