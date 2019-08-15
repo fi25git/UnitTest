1c7d8cce62d7f3631f6cd3548b035a4e01d0f21b
profzally <daniel.zallmanzig@gmx.de>
2019-08-15 10:36:42 +0200

    Kommt mit, der Wasserhahn funktioniert nun!

--- KaprekarZahlen/BadewanneMVC/ZallmanzigController.cs
+++ KaprekarZahlen/BadewanneMVC/ZallmanzigController.cs
@@ -12,6 +12,10 @@ namespace BadewanneMVC
     {
         public event EventHandler Input;
 
+        // Timer 
+        Timer timer = new Timer();
+        
+        
         public void ModelChanged(object sender, EventArgs e)
         {
 
@@ -24,29 +28,37 @@ namespace BadewanneMVC
 
         public ZallmanzigController()
         {
+            timer.Tick += Timer_Tick;
+
+            // Radio Button Offen
             RadioButton offen = new RadioButton();
             offen.Location = new Point(0, 10);
             offen.Text = "Offen";
             this.Controls.Add(offen);
             offen.CheckedChanged += Offen_CheckedChanged;
 
+            // Radio Button Geschlossen
             RadioButton geschlossen = new RadioButton();
             geschlossen.Location = new Point(0, 50);
             geschlossen.Text = "geschlossen";
+            geschlossen.Checked = true;
             this.Controls.Add(geschlossen);
             geschlossen.CheckedChanged += Geschlossen_CheckedChanged;
         }
 
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            ((IBadewanneModel)Input.GetInvocationList()[0].Target).fuellen(1);
+        }
+
         private void Geschlossen_CheckedChanged(object sender, EventArgs e)
         {
-            
+            timer.Stop();
         }
 
         private void Offen_CheckedChanged(object sender, EventArgs e)
         {
-            
+            timer.Start();
         }
-
-
     }
 }
