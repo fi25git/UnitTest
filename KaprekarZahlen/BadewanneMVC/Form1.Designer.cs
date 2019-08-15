b5a8943a1bcc4c6ca4e82c5332313353ad163223
steckart <stephan.eckart@comhard.de>
2019-08-15 13:14:18 +0200

    BadewanneZeigerView anpassen

--- KaprekarZahlen/BadewanneMVC/Form1.Designer.cs
+++ KaprekarZahlen/BadewanneMVC/Form1.Designer.cs
@@ -32,6 +32,7 @@ namespace BadewanneMVC
         {
             this.zallmanzigController1 = new BadewanneMVC.ZallmanzigController();
             this.thieleController1 = new BadewanneMVC.ThieleController();
+            this.badewannenZeigerView1 = new BadewanneMVC.BadewannenZeigerView();
             this.SuspendLayout();
             // 
             // zallmanzigController1
@@ -50,6 +51,16 @@ namespace BadewanneMVC
             this.thieleController1.TabIndex = 0;
             this.thieleController1.Text = "thieleController1";
             // 
+            // badewannenZeigerView1
+            // 
+            this.badewannenZeigerView1.Location = new System.Drawing.Point(484, 324);
+            this.badewannenZeigerView1.MaxVal = 300;
+            this.badewannenZeigerView1.Name = "badewannenZeigerView1";
+            this.badewannenZeigerView1.Size = new System.Drawing.Size(77, 73);
+            this.badewannenZeigerView1.TabIndex = 2;
+            this.badewannenZeigerView1.Text = "badewannenZeigerView1";
+            this.badewannenZeigerView1.Val = 90;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -57,6 +68,7 @@ namespace BadewanneMVC
             this.BackgroundImage = global::BadewanneMVC.res.badewanne2;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.badewannenZeigerView1);
             this.Controls.Add(this.zallmanzigController1);
             this.Controls.Add(this.thieleController1);
             this.DoubleBuffered = true;
@@ -73,6 +85,7 @@ namespace BadewanneMVC
 
         private ThieleController thieleController1;
         private ZallmanzigController zallmanzigController1;
+        private BadewannenZeigerView badewannenZeigerView1;
     }
 }
 
